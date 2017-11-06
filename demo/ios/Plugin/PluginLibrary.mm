b98590fb02c7dc9bdb820c295ced619b344a7e3a
Can Soykarafakılı <can.soykarafakili@gmail.com>
2017-11-06 16:54:41 +0100

    Initial code setup for iOS Banner

--- demo/ios/Plugin/PluginLibrary.mm
+++ demo/ios/Plugin/PluginLibrary.mm
@@ -10,50 +10,48 @@
 #include <CoronaRuntime.h>
 #import <UIKit/UIKit.h>
 
+#import <Pubnative/Pubnative.h>
+
 // ----------------------------------------------------------------------------
 
 class PluginLibrary
 {
-	public:
-		typedef PluginLibrary Self;
+public:
+    typedef PluginLibrary Self;
+    
+    static const char kName[];
+    static const char kEvent[];
+    
+    bool Initialize( CoronaLuaRef listener );
+    
+    CoronaLuaRef GetListener() const { return fListener; }
+    
+    static int Open( lua_State *L );
+    static Self *ToLibrary( lua_State *L );
+    static int init( lua_State *L );
+    static int load( lua_State *L );
+    static int show( lua_State *L );
+    static int hide( lua_State *L );
+    static int remove( lua_State *L );
+    
+protected:
+    PluginLibrary();
+    static int Finalizer( lua_State *L );
+    
+private:
+//    static void loadBanner( lua_State *L, NSString *appToken, NSString *placement, NSString *bannerID );
 
-	public:
-		static const char kName[];
-		static const char kEvent[];
-
-	protected:
-		PluginLibrary();
-
-	public:
-		bool Initialize( CoronaLuaRef listener );
-
-	public:
-		CoronaLuaRef GetListener() const { return fListener; }
-
-	public:
-		static int Open( lua_State *L );
-
-	protected:
-		static int Finalizer( lua_State *L );
-
-	public:
-		static Self *ToLibrary( lua_State *L );
-
-	public:
-		static int init( lua_State *L );
-		static int show( lua_State *L );
-
-	private:
-		CoronaLuaRef fListener;
+    
+    CoronaLuaRef fListener;
 };
 
 // ----------------------------------------------------------------------------
 
-// This corresponds to the name of the library, e.g. [Lua] require "plugin.library"
-const char PluginLibrary::kName[] = "plugin.library";
+// This corresponds to the name of the library, e.g. [Lua] require "plugin.pubnative.banner"
+const char PluginLibrary::kName[] = "plugin.pubnative.banner";
 
 // This corresponds to the event name, e.g. [Lua] event.name
-const char PluginLibrary::kEvent[] = "pluginlibraryevent";
+const char PluginLibrary::kEvent[] = "Banner";
 
 PluginLibrary::PluginLibrary()
 :	fListener( NULL )
@@ -63,120 +61,107 @@ PluginLibrary::PluginLibrary()
 bool
 PluginLibrary::Initialize( CoronaLuaRef listener )
 {
-	// Can only initialize listener once
-	bool result = ( NULL == fListener );
-
-	if ( result )
-	{
-		fListener = listener;
-	}
-
-	return result;
+    // Can only initialize listener once
+    bool result = ( NULL == fListener );
+    
+    if ( result )
+    {
+        fListener = listener;
+    }
+    
+    return result;
 }
 
 int
 PluginLibrary::Open( lua_State *L )
 {
-	// Register __gc callback
-	const char kMetatableName[] = __FILE__; // Globally unique string to prevent collision
-	CoronaLuaInitializeGCMetatable( L, kMetatableName, Finalizer );
-
-	// Functions in library
-	const luaL_Reg kVTable[] =
-	{
-		{ "init", init },
-		{ "show", show },
-
-		{ NULL, NULL }
-	};
-
-	// Set library as upvalue for each library function
-	Self *library = new Self;
-	CoronaLuaPushUserdata( L, library, kMetatableName );
-
-	luaL_openlib( L, kName, kVTable, 1 ); // leave "library" on top of stack
-
-	return 1;
+    // Register __gc callback
+    const char kMetatableName[] = __FILE__; // Globally unique string to prevent collision
+    CoronaLuaInitializeGCMetatable( L, kMetatableName, Finalizer );
+    
+    // Functions in library
+    const luaL_Reg kVTable[] =
+    {
+        { "init", init },
+        { "load", load },
+        { "show", show },
+        { "hide", hide },
+        { "remove", remove },
+        { NULL, NULL }
+    };
+    
+    // Set library as upvalue for each library function
+    Self *library = new Self;
+    CoronaLuaPushUserdata( L, library, kMetatableName );
+    
+    luaL_openlib( L, kName, kVTable, 1 ); // leave "library" on top of stack
+    
+    return 1;
 }
 
 int
 PluginLibrary::Finalizer( lua_State *L )
 {
-	Self *library = (Self *)CoronaLuaToUserdata( L, 1 );
-
-	CoronaLuaDeleteRef( L, library->GetListener() );
-
-	delete library;
-
-	return 0;
+    Self *library = (Self *)CoronaLuaToUserdata( L, 1 );
+    
+    CoronaLuaDeleteRef( L, library->GetListener() );
+    
+    delete library;
+    
+    return 0;
 }
 
 PluginLibrary *
 PluginLibrary::ToLibrary( lua_State *L )
 {
-	// library is pushed as part of the closure
-	Self *library = (Self *)CoronaLuaToUserdata( L, lua_upvalueindex( 1 ) );
-	return library;
+    // library is pushed as part of the closure
+    Self *library = (Self *)CoronaLuaToUserdata( L, lua_upvalueindex( 1 ) );
+    return library;
 }
 
 // [Lua] library.init( listener )
 int
 PluginLibrary::init( lua_State *L )
 {
-	int listenerIndex = 1;
-
-	if ( CoronaLuaIsListener( L, listenerIndex, kEvent ) )
-	{
-		Self *library = ToLibrary( L );
-
-		CoronaLuaRef listener = CoronaLuaNewRef( L, listenerIndex );
-		library->Initialize( listener );
-	}
-
-	return 0;
+    int listenerIndex = 1;
+    
+    if ( CoronaLuaIsListener( L, listenerIndex, kEvent ) )
+    {
+        Self *library = ToLibrary( L );
+        
+        CoronaLuaRef listener = CoronaLuaNewRef( L, listenerIndex );
+        library->Initialize( listener );
+    }
+    
+    return 0;
 }
 
-// [Lua] library.show( word )
-int
-PluginLibrary::show( lua_State *L )
+int PluginLibrary::load( lua_State *L )
 {
-	NSString *message = @"Error: Could not display UIReferenceLibraryViewController. This feature requires iOS 5 or later.";
-	
-	if ( [UIReferenceLibraryViewController class] )
-	{
-		id<CoronaRuntime> runtime = (id<CoronaRuntime>)CoronaLuaGetContext( L );
+    const char *appToken = lua_tostring(L, 1);
+    const char *placement = lua_tostring(L, 2);
 
-		const char kDefaultWord[] = "corona";
-		const char *word = lua_tostring( L, 1 );
-		if ( ! word )
-		{
-			word = kDefaultWord;
-		}
+    return 0;
+}
 
-		UIReferenceLibraryViewController *controller = [[[UIReferenceLibraryViewController alloc] initWithTerm:[NSString stringWithUTF8String:word]] autorelease];
+int PluginLibrary::show( lua_State *L )
+{
+    return 0;
+}
 
-		// Present the controller modally.
-		[runtime.appViewController presentViewController:controller animated:YES completion:nil];
+int PluginLibrary:: hide( lua_State *L )
+{
+    return 0;
+}
 
-		message = @"Success. Displaying UIReferenceLibraryViewController for 'corona'.";
-	}
-
-	Self *library = ToLibrary( L );
-
-	// Create event and add message to it
-	CoronaLuaNewEvent( L, kEvent );
-	lua_pushstring( L, [message UTF8String] );
-	lua_setfield( L, -2, "message" );
-
-	// Dispatch event to library's listener
-	CoronaLuaDispatchEvent( L, library->GetListener(), 0 );
-
-	return 0;
+int PluginLibrary:: remove( lua_State *L )
+{
+    return 0;
 }
 
 // ----------------------------------------------------------------------------
 
-CORONA_EXPORT int luaopen_plugin_library( lua_State *L )
+CORONA_EXPORT int luaopen_plugin_pubnative_banner( lua_State *L )
 {
-	return PluginLibrary::Open( L );
+    return PluginLibrary::Open( L );
 }
