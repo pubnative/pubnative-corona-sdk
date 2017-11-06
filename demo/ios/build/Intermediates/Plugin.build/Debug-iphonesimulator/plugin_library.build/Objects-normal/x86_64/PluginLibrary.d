9d109c2b1299eb65d338ea2d042e490818484796
Can Soykarafakılı <can.soykarafakili@gmail.com>
2017-11-06 10:52:08 +0100

    Started working on Corona iOS Banner

---
+++ demo/ios/build/Intermediates/Plugin.build/Debug-iphonesimulator/plugin_library.build/Objects-normal/x86_64/PluginLibrary.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Plugin/PluginLibrary.mm \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Plugin/PluginLibrary.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/Corona/CoronaLua.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/Corona/CoronaMacros.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/Corona/CoronaLog.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/lua.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/luaconf.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/lauxlib.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/lualib.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/ios/include/Corona/CoronaRuntime.h
