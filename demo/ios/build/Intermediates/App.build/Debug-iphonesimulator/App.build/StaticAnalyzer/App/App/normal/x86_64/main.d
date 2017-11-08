8238031ddd94b37a737f1fc7d15960a78470476c
Can Soykarafakılı <can.soykarafakili@gmail.com>
2017-11-08 15:59:37 +0100

    Continue working on Corona iOS Banner
    The only thing that is remained so far is the memory issue.

--- demo/ios/build/Intermediates/App.build/Debug-iphonesimulator/App.build/StaticAnalyzer/App/App/normal/x86_64/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Prefix.pch \
-  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/main.mm \
-  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/ios/include/Corona/CoronaApplicationMain.h \
-  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/AppCoronaDelegate.h \
-  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/ios/include/Corona/CoronaDelegate.h
