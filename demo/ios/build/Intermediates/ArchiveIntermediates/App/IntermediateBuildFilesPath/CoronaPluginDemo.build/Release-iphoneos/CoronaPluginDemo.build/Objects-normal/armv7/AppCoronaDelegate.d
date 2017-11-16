ac1d1c68ce154abbfe739d96fe3973d110fc8d7f
Can Soykarafakılı <can.soykarafakili@gmail.com>
2017-11-16 15:56:40 +0100

    v:1.0 Build:2

---
+++ demo/ios/build/Intermediates/ArchiveIntermediates/App/IntermediateBuildFilesPath/CoronaPluginDemo.build/Release-iphoneos/CoronaPluginDemo.build/Objects-normal/armv7/AppCoronaDelegate.d
@@ -0,0 +1,22 @@
+dependencies: \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Prefix.pch \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/AppCoronaDelegate.mm \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/AppCoronaDelegate.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/ios/include/Corona/CoronaDelegate.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/ios/include/Corona/CoronaRuntime.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/Corona/CoronaLua.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/Corona/CoronaMacros.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/Corona/CoronaLog.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/lua.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/luaconf.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/lauxlib.h \
+  /Users/cansoykarafakili/Library/Application\ Support/Corona/Native/Corona/shared/include/lua/lualib.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Fabric.framework/Headers/Fabric.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Fabric.framework/Headers/FABAttributes.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Crashlytics.framework/Headers/Crashlytics.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Crashlytics.framework/Headers/CLSAttributes.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Crashlytics.framework/Headers/CLSLogging.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Crashlytics.framework/Headers/CLSReport.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Crashlytics.framework/Headers/CLSStackFrame.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Crashlytics.framework/Headers/Answers.h \
+  /Users/cansoykarafakili/Documents/Pubnative\ Projects/Corona\ Plugin/pubnative-corona-sdk/demo/ios/Crashlytics.framework/Headers/ANSCompatibility.h
